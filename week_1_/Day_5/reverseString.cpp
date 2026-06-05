a11d428b1034c10d66b76ce4565e22701d28ed4d
yash8901618 <mishrayash890@gmail.com>
2026-06-05 21:40:56 +0530

    week 1 solution

---
+++ week_1_/Day_5/reverseString.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using  namespace std;
+ 
+ void reverseString(vector<char>& s) {
+        int left = 0;
+        int right = s.size() - 1;
+
+        while (left < right) {
+            swap(s[left], s[right]);
+            left++;
+            right--;
+        }
+    }
